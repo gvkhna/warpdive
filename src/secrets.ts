e1eacbbb583a3a99a1c9d9583da8ec5bd4d0c87c
Gaurav Khanna <gauravk92@gmail.com>
2024-05-31 12:01:58 -0700

    add additional logging for secret handling

--- src/secrets.ts
+++ src/secrets.ts
@@ -1,10 +1,7 @@
 // Astro doesn't reflect cloudflare env secrets correctly
-
 import {Context} from 'hono'
 
 export function getSecretAstro(key: string, locals: App.Locals) {
-  console.log('import env', import.meta.env)
-  console.log('astro env', locals.runtime.env)
   if (import.meta && import.meta.env && import.meta.env[key]) {
     return import.meta.env[key]
   } else if (locals && locals.runtime && locals.runtime.env && locals.runtime.env[key]) {
@@ -15,9 +12,6 @@ export function getSecretAstro(key: string, locals: App.Locals) {
 }
 
 export function getSecretPlatform(key: string, ctx: Context) {
-  const importMetaEnv = import.meta.env
-  const contextEnv = ctx.env
-
   if (import.meta && import.meta.env && import.meta.env[key]) {
     return import.meta.env[key]
   } else if (ctx && ctx.env[key]) {
