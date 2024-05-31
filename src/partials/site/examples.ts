5b3163f7bad1b4429c20085e268a9eadd7483361
Gaurav Khanna <gauravk92@gmail.com>
2024-05-31 12:57:11 -0700

    update example links

--- src/partials/site/examples.ts
+++ src/partials/site/examples.ts
@@ -10,50 +10,50 @@ export const Examples: ExampleItem[] = [
   {
     id: 'node',
     name: 'node:latest',
-    url: 'https://www.warpdive.xyz/share/01hz593s1qj1c1epghmnv66vdn/',
+    url: 'https://www.warpdive.xyz/share/01hz83hcecw8r0fvcnqgd5wjym/',
     image: 'node-latest.png',
     default: true
   },
   {
     id: 'ubuntu',
     name: 'ubuntu:noble',
-    url: 'https://www.warpdive.xyz/share/01hz598eawqwkdvetayjankaps/',
+    url: 'https://www.warpdive.xyz/share/01hz83bn4b9j96mjwafv1b70f1/',
     image: 'ubuntu-latest.png'
   },
   {
     id: 'alpine',
     name: 'alpine:latest',
-    url: 'https://www.warpdive.xyz/share/01hz58ztjzh93jgngjtt581fyj/',
+    url: 'https://www.warpdive.xyz/share/01hz83k19mp010dkx2yr6bpwhp/',
     image: 'alpine-latest.png'
   },
   {
     id: 'postgres',
     name: 'postgres:latest',
-    url: 'https://www.warpdive.xyz/share/01hz594hj6bekjf5q98verwczf/',
+    url: 'https://www.warpdive.xyz/share/01hz83g7k1h6tcf5hkm30kr37e/',
     image: 'postgres-latest.png'
   },
   {
     id: 'mysql',
     name: 'mysql:latest',
-    url: 'https://www.warpdive.xyz/share/01hz592agzagwn15sxr956q1yp/',
+    url: 'https://www.warpdive.xyz/share/01hz83j8a4d1yh05s8pd1cp7m1/',
     image: 'mysql-latest.png'
   },
   {
     id: 'rails',
     name: 'rails:latest',
-    url: 'https://www.warpdive.xyz/share/01hz596s47kbsmgcr7nh0k2j6n/',
+    url: 'https://www.warpdive.xyz/share/01hz83e89vvkvv9bsdg0f6rm24/',
     image: 'rails-latest.png'
   },
   {
     id: 'redis',
     name: 'redis:latest',
-    url: 'https://www.warpdive.xyz/share/01hz597h1bqks51e3mzkcmxj4q/',
+    url: 'https://www.warpdive.xyz/share/01hz83d1rj1x79ph96ywgycv2x/',
     image: 'redis-latest.png'
   },
   {
     id: 'python',
     name: 'python:latest',
-    url: 'https://www.warpdive.xyz/share/01hz595nvxstzkv4rxxzae2z6c/',
+    url: 'https://www.warpdive.xyz/share/01hz83fbw8pxp8g89c6vm3gysg/',
     image: 'python-latest.png'
   }
 ]
